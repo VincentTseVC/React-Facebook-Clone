acf32297181b171a6b3fa2f78dc47e994a5ad574
VincentTse <from_vincent@hotmail.com>
2021-08-21 22:40:02 +0800

    added google login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,10 @@ export default NextAuth({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     // ...add more providers here
   ],
 
